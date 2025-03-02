ff1e968f6f3775a9ca77ef9a22518034c6898dec
Alex <xelawe@users.noreply.github.com>
2025-03-02 13:13:25 +0100

    Update utils_mqtt.py

--- xsense/utils_mqtt.py
+++ xsense/utils_mqtt.py
@@ -35,18 +35,24 @@ def get_mqttenv():
     raise ValueError('MQTT environments not provided')
 
 
-def mqtt_environment(env: XSenseBase):
+def mqtt_environment(env: XSenseBase, client, topic):
     for h_id, h in env.houses.items():
         print(f'----[ {h.name} ({h_id}) ]-----------------')
         for s_id, s in h.stations.items():
-            mqtt_device(s)
+#            mqtt_device(s, client, topic)
+            client.publish(f'{topic}/{h.name}/{s.name}/serial',f'{s.sn}')
+            client.publish(f'{topic}/{h.name}/{s.name}/online',f'{"yes" if s.online else "no"}')
+            client.publish(f'{topic}/{h.name}/{s.name}/values',f'{s.data}')            
             print(f'# {s.name} ({s_id})')
             for d_id, d in s.devices.items():
-                mqtt_device(d)
+#                mqtt_device(d)
+                client.publish(f'{topic}/{h.name}/{d.name}/serial',f'{d.sn}')
+                client.publish(f'{topic}/{h.name}/{d.name}/online',f'{"yes" if d.online else "no"}')
+                client.publish(f'{topic}/{h.name}/{d.name}/values',f'{d.data}')                
 
 
-def mqtt_device(d):
+def mqtt_device(d, client, topic):
     print(f'{d.name} ({d.type}):')
     print(f'  serial  : {d.sn}')
     print(f'  online  : {"yes" if d.online else "no"}')
-    print(f'  values  : {d.data}')
+    print(f'  values  : {d.data}')  
