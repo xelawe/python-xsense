c8f3934e9944096b12d557c2f0eef52fba5c9ea6
Alex <xelawe@users.noreply.github.com>
2025-03-02 14:21:46 +0100

    Update asyncmqtttest.py

--- asyncmqtttest.py
+++ asyncmqtttest.py
@@ -20,7 +20,7 @@ async def run(username: str, password: str):
 
     dump_environment(api)
 
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=str(uuid.uuid4()))
+    client = mqtt.Client(client_id=str(uuid.uuid4()))
     mqtthost, mqttusr, mqttpwd, mqtttopic = get_mqttenv()
     client.username_pw_set(mqttusr,mqttpwd)  
     client.on_connect = on_connect
