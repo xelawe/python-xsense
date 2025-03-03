48a809354be9aefd00ea1f202a8262e8deeb146c
Alex <xelawe@users.noreply.github.com>
2025-03-03 10:06:15 +0100

    Update asyncmqttsrvc.py

--- asyncmqttsrvc.py
+++ asyncmqttsrvc.py
@@ -16,23 +16,23 @@ async def run(username: str, password: str):
     client.loop_start()
   
     api = AsyncXSense()
-
-  while True:
-    await api.init()
-    await api.login(username, password)
-    await api.load_all()
-
-    for _, h in api.houses.items():
-        await api.get_house_state(h)
-        for _, s in h.stations.items():
-            await api.get_station_state(s)
-            await api.get_state(s)
-
-    dump_environment(api)
-    mqtt_environment(api, client, mqtttopic)
-
-#    client.loop_stop()
-    time.sleep(1*60)
+  
+    while True:
+      await api.init()
+      await api.login(username, password)
+      await api.load_all()
+  
+      for _, h in api.houses.items():
+          await api.get_house_state(h)
+          for _, s in h.stations.items():
+              await api.get_station_state(s)
+              await api.get_state(s)
+  
+      dump_environment(api)
+      mqtt_environment(api, client, mqtttopic)
+  
+  #    client.loop_stop()
+      time.sleep(1*60)
   
 username, password = get_credentials()
 asyncio.run(run(username, password))
