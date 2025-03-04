aec60fc04248b17f51fca85a991750c6ec906fad
Alex <xelawe@users.noreply.github.com>
2025-03-04 09:47:59 +0100

    Update utils_mqtt.py

--- xsense/utils_mqtt.py
+++ xsense/utils_mqtt.py
@@ -55,4 +55,6 @@ def mqtt_device(d, d_id, client, topic):
     client.publish(f'{topic}/{d.name}/id',f'{d_id}')            
     client.publish(f'{topic}/{d.name}/serial',f'{d.sn}')
     client.publish(f'{topic}/{d.name}/online',f'{"yes" if d.online else "no"}')
-    client.publish(f'{topic}/{d.name}/values',f'{d.data}')        
+    json=d.data.replace("\'","\"")   
+#    client.publish(f'{topic}/{d.name}/values',f'{d.data}')        
+    client.publish(f'{topic}/{d.name}/values',f'{d.data}')    
